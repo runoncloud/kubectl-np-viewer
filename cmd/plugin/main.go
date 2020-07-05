f008737e1e255c6c82830b633ba83c8b1347db3e
jeanpbond <jeanp.bond.dev@gmail.com>
2020-07-05 12:56:53 -0400

    Add the github action for the krew-release-bot

--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/runoncloud/kubectl-np-viewer/cmd/plugin/cli"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // required for GKE
+	_ "k8s.io/client-go/plugin/pkg/client/auth" // required for cloud providers
 )
 
 func main() {
